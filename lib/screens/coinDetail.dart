37a6c93b5531b321546855fb723d82c30e8c82e9
Mostafa Bahri <mostafabahri.dev@gmail.com>
2018-04-24 07:40:09 +0430

    implement skeleton of new design for coinDetail

--- lib/screens/coinDetail.dart
+++ lib/screens/coinDetail.dart
@@ -8,55 +8,136 @@ class CoinItemDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new Center(
-        child: new Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: new Container(
-              child: new Column(
-                  children: [
-                    new Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        new Text(coin.name,
-                            style: new TextStyle(
-                                fontSize: 40.0,
-                                color: Colors.grey[600]
-                            )),
-                        new Text(coin.symbol,
-                            style: new TextStyle(
-                                fontSize: 35.0,
-                                color: Colors.blueGrey
-                            )),
-                      ],
-                    ),
-                    new Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        new Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: new Container(
-                            child:
-                            new Text("\$${coin.priceUsd}",
-                              style: new TextStyle(
-                                fontSize: 25.0,
-                                fontWeight: FontWeight.w600,
-                              ),
+
+    String rankText() {
+      if (coin.rank == 1)
+        return '1st';
+      else if (coin.rank == 2)
+        return '2nd';
+      return '${coin.rank}th';
+    }
+
+    return new Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+      child: new Container(
+          child: new Column( // top level column
+              children: [
+                new Row(
+                  // rank, name, price
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    new Column(children: <Widget>[
+                      new Text('Ranked ${rankText()}'),
+                      new Padding(
+                        padding: const EdgeInsets.only(left: 1.0),
+                        child: new Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            new Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              // name, symbol
+                              children: <Widget>[
+                                new Text('${coin.name}'),
+                                new Text('${coin.symbol}')
+                              ],
                             ),
-                          ),
-                        )
+                            new Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                new Text('${coin.priceUSD} USD'),
+                                new Text('${coin.priceBTC} BTC')
+                              ],
+                            ) // usd and btc price
+                          ],
+                        ),
+                      )
+                    ])
+                  ],
+                ),
+                new Divider(),
+                new Row(
+                  // cap, volume
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    new Column(
+                      children: <Widget>[
+                        new Text('Market Cap'),
+                        new Text('\$${coin.marketCapUSD} USD')
                       ],
                     ),
                     new Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        new Text('Volume'),
+                        new Text('\$ ${coin.volume24H} USD')
+                      ],
+                    ),
+                  ],
+                ),
+                new Divider(),
+                new Row(
+                  // percentage changes, supply stat
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      new Column(
                         children: <Widget>[
-                          new Text("1 hour: ${coin.change1Hour}%"),
-                          new Text("24 hour: ${coin.change24Hour}%"),
-                        ]
-                    )
-                  ]
-              )
-          ),
-        ));
+                          new Row(
+                            children: <Widget>[
+                              new Text('1 Hour'),
+                              new Text('${coin.change1Hour}%'),
+                              new Icon(coin.change1Hour >= 0
+                                  ? Icons.arrow_upward
+                                  : Icons.arrow_downward)
+                            ],
+                          ),
+                          new Row(
+                            children: <Widget>[
+                              new Text('24 Hour'),
+                              new Text('${coin.change24Hour}%'),
+                              new Icon(coin.change24Hour >= 0
+                                  ? Icons.arrow_upward
+                                  : Icons.arrow_downward)
+                            ],
+                          ),
+                          new Row(
+                            children: <Widget>[
+                              new Text('7 Days'),
+                              new Text('${coin.change7Days}%'),
+                              new Icon(coin.change7Days >= 0
+                                  ? Icons.arrow_upward
+                                  : Icons.arrow_downward)
+                            ],
+                          )
+                        ],
+                      ),
+                      new Column(
+                        children: <Widget>[
+                          new Column(
+                            children: <Widget>[
+                              new Text('available supply'),
+                              new Text('${coin.availableSupply} USD'),
+                            ],
+                          ), // available supply
+                          new Column(
+                            children: <Widget>[
+                              new Text('total supply'),
+                              new Text('${coin.totalSupply} USD'),
+                            ],
+                          ) // total supply
+                        ],
+                      ),
+                    ]
+                ),
+                new Divider(),
+                new Row( // chart(s)
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      new Placeholder(
+                        fallbackHeight: 150.0,
+                      )
+                    ])
+              ])),
+    );
   }
 }

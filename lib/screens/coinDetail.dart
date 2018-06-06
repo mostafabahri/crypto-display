e78638aa3d5b12f0b413d079dcef90ecf0a03dd6
Mostafa Bahri <mostafabahri.dev@gmail.com>
2018-06-07 02:54:23 +0430

    improve hourly section ui and base padding

--- lib/screens/coinDetail.dart
+++ lib/screens/coinDetail.dart
@@ -16,7 +16,12 @@ class CoinItemDetail extends StatelessWidget {
   final Coin coin;
 
   CoinItemDetail(Coin coin) : coin = coin;
-
+  
+  Divider _rowDivider(){
+    return new Divider(
+                height: 45.0, 
+              );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +40,7 @@ class CoinItemDetail extends StatelessWidget {
 
 
     return new Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+      padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
       child: new Container(
           child: new Column( // top level column
               children: [
@@ -48,7 +53,10 @@ class CoinItemDetail extends StatelessWidget {
                       child: new Column(children: <Widget>[
                         new Row(
                           children: <Widget>[
-                            new Text('Ranked ${_rankText(coin.rank)}'),
+                            new Text('Ranked ${_rankText(coin.rank)}', style: new TextStyle(
+                              fontSize: 20.0,
+                              color: Colors.blueGrey
+                            ),),
                           ],
                         ),
                         new Row(
@@ -67,13 +75,13 @@ class CoinItemDetail extends StatelessWidget {
                                 ],
                               ),
                             ),
-                            // new Container(
-                            //     width: 10.0,
-                            //     height: 20.0,
-                            //     decoration: new BoxDecoration(
-                            //       border: new Border(left : new BorderSide(color: Colors.black38, width: 2.0))
-                            //     ),
-                            // ),
+                            new Container(
+                                width: 10.0,
+                                height: 40.0,
+                                decoration: new BoxDecoration(
+                                  border: new Border(left : new BorderSide(color: Colors.black38, width: 2.0))
+                                ),
+                            ),
                             new Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: <Widget>[
@@ -89,7 +97,7 @@ class CoinItemDetail extends StatelessWidget {
                     ),
                   ],
                 ),
-                new Divider(),
+                _rowDivider(),
 
                 new Row(
                   // cap, volume
@@ -112,36 +120,45 @@ class CoinItemDetail extends StatelessWidget {
                     ),
                   ],
                 ),
-                new Divider(),
+                _rowDivider(),
                 new Row(
                   // percentage changes, supply stat
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: <Widget>[
-                      new Column(
+                      new Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
                         children: <Widget>[
-                          new Row(
+                          new Column(
+                            crossAxisAlignment : CrossAxisAlignment.start,
                             children: <Widget>[
-                              new Text('1 Hour'),
-                              new Text('${coin.change1Hour}%'),
-                              new Icon(coin.change1Hour >= 0
-                                  ? Icons.arrow_upward
-                                  : Icons.arrow_downward)
+                              new Text('1 Hour', style: secondaryTextStyle,),
+                              new Text('24 Hour', style: secondaryTextStyle,),
+                              new Text('7 Days', style: secondaryTextStyle,),
+
                             ],
                           ),
-                          new Row(
+                          new Container(
+                            width: 15.0,
+                          ),
+                          new Column(
+                            crossAxisAlignment : CrossAxisAlignment.end,                            
                             children: <Widget>[
-                              new Text('24 Hour'),
-                              new Text('${coin.change24Hour}%'),
+                              new Text('${coin.change1Hour}%', style: secondaryTextStyle,),
+                              new Text('${coin.change24Hour}%', style: secondaryTextStyle,),
+                              new Text('${coin.change7Days}%', style: secondaryTextStyle,),
+
+                            ],
+                          ),
+                          new Column(
+                            children: <Widget>[
+                              new Icon(coin.change1Hour >= 0
+                                  ? Icons.arrow_upward
+                                  : Icons.arrow_downward ),
                               new Icon(coin.change24Hour >= 0
                                   ? Icons.arrow_upward
                                   : Icons.arrow_downward)
-                            ],
-                          ),
-                          new Row(
-                            children: <Widget>[
-                              new Text('7 Days'),
-                              new Text('${coin.change7Days}%'),
+                            ,
                               new Icon(coin.change7Days >= 0
                                   ? Icons.arrow_upward
                                   : Icons.arrow_downward)
@@ -170,7 +187,7 @@ class CoinItemDetail extends StatelessWidget {
                         ],
                       ),
                     ]),
-                new Divider(),
+                _rowDivider(),
                 new Row(
                   // chart(s)
                     mainAxisAlignment: MainAxisAlignment.center,

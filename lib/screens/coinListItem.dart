334256c5b759d7077e613df510953ff9566688e3
Mostafa Bahri <mostafabahri.dev@gmail.com>
2018-06-10 00:32:47 +0430

    use coin name as appBar name

--- lib/screens/coinListItem.dart
+++ lib/screens/coinListItem.dart
@@ -5,46 +5,35 @@ import 'package:crypto_display/screens/coinDetail.dart';
 class CoinListItem extends StatelessWidget {
   final Coin coin;
 
-  CoinListItem(Coin coin)
-      : coin = coin;
+  CoinListItem(Coin coin) : coin = coin;
 
   void _onTap(BuildContext context) {
-    Navigator.of(context).push(
-        new MaterialPageRoute(builder: (context) {
-          return new Scaffold(
-            appBar: new AppBar(
-                title: new Text("Coin Stats")
-            ),
-            body: new CoinItemDetail(this.coin),
-          );
-        })
-    );
+    Navigator.of(context).push(new MaterialPageRoute(builder: (context) {
+      return new Scaffold(
+        appBar: new AppBar(title: new Text(this.coin.name)),
+        body: new CoinItemDetail(this.coin),
+      );
+    }));
   }
 
-  Icon _downRedIcon() =>
-      new Icon(
+  Icon _downRedIcon() => new Icon(
         Icons.arrow_drop_down,
         color: Colors.red,
       );
 
-
-  Icon _upGreenIcon() =>
-      new Icon(
+  Icon _upGreenIcon() => new Icon(
         Icons.arrow_drop_up,
         color: Colors.green,
       );
 
   @override
   Widget build(BuildContext context) {
-    return
-      new ListTile(
-        leading: new Icon(Icons.local_atm),
-        title: new Text(coin.name),
-        trailing: coin.change1Hour >= 0
-            ? _upGreenIcon()
-            : _downRedIcon(),
-        subtitle: new Text("\$${coin.priceUSD}"),
-        onTap: () => _onTap(context),
-      );
+    return new ListTile(
+      leading: new Icon(Icons.local_atm),
+      title: new Text(coin.name),
+      trailing: coin.change1Hour >= 0 ? _upGreenIcon() : _downRedIcon(),
+      subtitle: new Text("\$${coin.priceUSD}"),
+      onTap: () => _onTap(context),
+    );
   }
 }
